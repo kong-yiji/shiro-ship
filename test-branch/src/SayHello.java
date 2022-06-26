894908aa5b28279d08da69770a6c64c08db224d4
尹宏基 <2066902210@qq.com>
2022-06-26 08:53:38 +0800

    制造冲突

--- test-branch/src/SayHello.java
+++ test-branch/src/SayHello.java
@@ -1,6 +1,6 @@
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
-        System.out.println("第二个分支捣乱");
+        System.out.println("制造冲突");
     }
 }

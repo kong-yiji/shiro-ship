c09b93bdb0e729fce74ecc9e3cf393302966b20c
尹宏基 <2066902210@qq.com>
2022-06-26 09:01:21 +0800

    分支方法

--- test-branch/src/SayHello.java
+++ test-branch/src/SayHello.java
@@ -1,4 +1,7 @@
 public class SayHello {
+    public static void conflict(int a,int b){
+        System.out.println("我是分支");
+    }
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("制造冲突");

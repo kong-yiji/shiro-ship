abefdfcde8e398e85f2a72aaa96dce400dd19160
尹宏基 <2066902210@qq.com>
2022-06-26 09:01:21 +0800

    試試

--- test-branch/src/SayHello.java
+++ test-branch/src/SayHello.java
@@ -1,11 +1,5 @@
 public class SayHello {
-    public  static void conflict(int a){
-        System.out.println("我是主干");
-    }
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
-        System.out.println("制造冲突");
-        System.out.println("还");
-        System.out.println("代码");
+
     }
 }

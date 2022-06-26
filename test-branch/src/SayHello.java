82fe5e56daf9b0f4a07691f76a4dbfa5177434a0
尹宏基 <2066902210@qq.com>
2022-06-26 09:02:46 +0800

    主干方法

--- test-branch/src/SayHello.java
+++ test-branch/src/SayHello.java
@@ -1,4 +1,7 @@
 public class SayHello {
+    public  static void conflict(int a){
+        System.out.println("我是主干");
+    }
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("制造冲突");
